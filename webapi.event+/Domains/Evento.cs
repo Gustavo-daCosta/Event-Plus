0489df2b8b99ffcdf15eb0654b089400c80ec084
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-09-18 17:26:05 -0300

    API criada e Domain das entidades feitas

---
+++ webapi.event+/Domains/Evento.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.event_.Domains
+{
+    [Table(nameof(Evento))]
+    public class Evento
+    {
+        [Key]
+        public Guid IdEvento { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(150)")]
+        [Required(ErrorMessage = "Nome do Evento é obrigatório")]
+        public string? NomeEvento { get; set; }
+
+        [Column(TypeName = "DATETIME")]
+        [Required(ErrorMessage = "Data do Evento é obrigatória!")]
+        public DateTime DataEvento { get; set; }
+
+        [Column(TypeName = "VARCHAR(200)")]
+        [Required(ErrorMessage = "Descrição do Evento é obrigatório")]
+        public string? Descricao { get; set; }
+
+        // Ref. TipoEvento - FK
+        [Required(ErrorMessage = "Tipo de Evento é obrigatório!")]
+        public Guid IdTipoEvento { get; set; }
+
+        [ForeignKey(nameof(IdTipoEvento))]
+        public TipoEvento? TipoEvento { get; set; }
+
+        // Ref. Instituição - FK
+        [Required(ErrorMessage = "Instituicao é obrigatória!")]
+        public Guid IdInstituicao { get; set; }
+
+        [ForeignKey(nameof(IdInstituicao))]
+        public Instituicao? Instituicao { get; set; }
+    }
+}
---
+++ webapi.event+/Domains/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.event_.Domains
+{
+    [Table(nameof(Usuario))]
+    [Index(nameof(Email), IsUnique = true)]
+    public class Usuario
+    {
+        [Key]
+        public Guid IdUsuario { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Nome do Usuário é obrigatório!")]
+        public string? Nome { get; set; }
+
+        [Column(TypeName = "Email do Usuário é obrigatório")]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Column(TypeName = "CHAR(60)")]
+        [Required(ErrorMessage = "Senha do Usuário é obrigatória!")]
+        [StringLength(60, MinimumLength = 8, ErrorMessage = "A Senha deve ter entre 6 e 60 caracteres")]
+        public string? Senha { get; set; }
+
+        // Ref. Tabela TipoUsuario = FK
+        [Required(ErrorMessage = "O Tipo do Usuário é obrigatório")]
+        public Guid IdTipoUsuario { get; set; }
+
+        [ForeignKey(nameof(IdTipoUsuario))]
+        public TipoUsuario? TipoUsuario { get; set; }
+    }
+}

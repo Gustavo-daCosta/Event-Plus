e56b82bd3b71e316ac8aa6dff8b1a52aa204db69
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-12-08 17:23:23 -0300

    React: Rota da API modificada

--- React Event+/src/Services/Service.js
+++ React Event+/src/Services/Service.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const apiPort = "7118";
-const localApi = `https://localhost:${apiPort}/api`;
+// const apiPort = "7118";
+// const localApi = `https://localhost:${apiPort}/api`;
+const externalApi = "https://eventplusapiwebgustavo.azurewebsites.net/api";
 // const externalApi = null;
 
 const api = axios.create({
-    baseURL : localApi
+    baseURL : externalApi
 });
 
 export default api;
